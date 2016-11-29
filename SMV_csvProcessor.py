5b8702835255540ada67cd95289a04da0e186eb5
Jamie Campbell <Jamie3.14159@gmail.com>
2016-11-29 12:14:43 -0800

    Added open and save file dialogs for real

--- SMV_csvProcessor.py
+++ SMV_csvProcessor.py
@@ -1,22 +1,28 @@
 import csv
-csvfliename = 'Patientsrxnt.csv'
-csvfile = open(csvfliename, 'r', newline='')
-csvreader = csv.reader(csvfile)
-newcsvfilename = csvfliename.split('.')[0]+'_processed.csv'
-newcsvfile = open(newcsvfilename, 'w',  newline='')
-csvwriter = csv.writer(newcsvfile, quoting=csv.QUOTE_MINIMAL)
-csvheader = ["LastName", "FirstName", "MiddleInitial"]
-for x in csvreader.__next__()[1:]:
-	csvheader.append(x)
-csvwriter.writerow(csvheader)
+import tkinter as tk
+from tkinter import filedialog
+root = tk.Tk()
+root.withdraw()
 
-for row in csvreader:
-	name = row[0].replace(',', '').split(' ')
-	lName = name[0].upper()
-	fName = name[1].upper()
-	mName = (name[2].upper() if len(name) == 3 else '')
-	newrow = [lName, fName, mName]
-	[newrow.append(x) for x in row[1:]]
-	csvwriter.writerow(newrow)
-	# print(newrow)
+csvfliename = filedialog.askopenfilename(title="Open File to Process")
+newcsvfilename = filedialog.asksaveasfilename(title="Save File As", defaultextension = ".csv")
+
+if csvfliename and newcsvfilename:
+	csvfile = open(csvfliename, 'r', newline='')
+	csvreader = csv.reader(csvfile)
+	newcsvfile = open(newcsvfilename, 'w',  newline='')
+	csvwriter = csv.writer(newcsvfile, quoting=csv.QUOTE_MINIMAL)
+	csvheader = ["LastName", "FirstName", "MiddleInitial"]
+	for x in csvreader.__next__()[1:]:
+		csvheader.append(x)
+	csvwriter.writerow(csvheader)
+
+	for row in csvreader:
+		name = row[0].replace(',', '').split(' ')
+		lName = name[0].upper()
+		fName = name[1].upper()
+		mName = (name[2].upper() if len(name) == 3 else '')
+		newrow = [lName, fName, mName]
+		[newrow.append(x) for x in row[1:]]
+		csvwriter.writerow(newrow)
 
